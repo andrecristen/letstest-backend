a36ad9c05031b0c90fbd7d6f8a3e7d6f72e43c2d
André Cristen <30880513+andrecristen@users.noreply.github.com>
2024-05-13 21:36:09 -0300

    LST-41

--- src/testExecution/testExecution.router.ts
+++ src/testExecution/testExecution.router.ts
@@ -7,6 +7,34 @@ import * as TestExecutionService from "./testExecution.service";
 
 export const testExecutionRouter = express.Router();
 
+testExecutionRouter.get("/test-case/:testCaseId", async (request: Request, response: Response) => {
+    //@todo adiconar validações para ver se usuário está no projeto
+    const testCaseId: number = parseInt(request.params.testCaseId);
+    try {
+        const testExecutions = await TestExecutionService.findBy({ testCaseId });
+        if (testExecutions) {
+            return response.status(200).json(testExecutions);
+        }
+        return response.status(404).json("Nenhuma execução de testes encontrada");
+    } catch (error: any) {
+        return response.status(500).json(error.message);
+    }
+});
+
+testExecutionRouter.get("/:id", async (request: Request, response: Response) => {
+    //@todo adiconar validações para ver se usuário está no projeto
+    const id: number = parseInt(request.params.id);
+    try {
+        const testExecution = await TestExecutionService.find(id);
+        if (testExecution) {
+            return response.status(200).json(testExecution);
+        }
+        return response.status(404).json("Execução de testes não encontrada");
+    } catch (error: any) {
+        return response.status(500).json(error.message);
+    }
+});
+
 testExecutionRouter.post("/:testCaseId", token.authMiddleware, body("testTime").isNumeric(), body("data").isObject(), async (request: Request, response: Response) => {
     const errors = validationResult(request);
     if (!errors.isEmpty()) {
